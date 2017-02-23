00e7954be8838826e941c4901435ed23863234b8
Jánosi Dávid <deveneszlol@gmail.com>
2017-02-23 10:51:10 +0100

    Update kontener.sh

--- kontener.sh
+++ kontener.sh
@@ -2,6 +2,7 @@
 lxc init ubuntu:16.04 dawe
 lxc start dawe
 sleep 30
+lxc exec dawe -- apt install mc --yes
 lxc exec dawe -- apt update --yes
 lxc exec dawe -- apt upgrade --yes
-lxc exec dawe -- apt install mc --yes
+
